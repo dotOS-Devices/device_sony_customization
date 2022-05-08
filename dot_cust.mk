701fa8514180ed6e542c29055dcded3da8fc6f45
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-05-08 14:50:32 +0200

    dot_cust.mk: Rename from pe_cust.mk

--- dot_cust.mk
+++ dot_cust.mk
@@ -1,6 +1,6 @@
 # Copyright (C) 2019 AngeloGioacchino Del Regno <kholk11@gmail.com>
 #
-# PixelExperience specific customization for Sony Open Devices
+# DotOS specific customization for Sony Open Devices
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
